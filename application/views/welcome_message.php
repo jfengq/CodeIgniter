13d39b1a5882dd60ba1891203a4afd0293aa59b9
micro <648580211@qq.com>
2016-08-07 21:35:08 +0800

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<a href="/cheng_jiao">成交量列表</a>
 	</div>
 	<div class="form-group">
-		<a href="/tui_jian?day=3&chengjiao0=0.6&chengjiao1=1&zuidi=5&zuigao=25&zhangfu0=5&zhangfu1=5&zhangfu2=8&zhangfu3=10">今日推荐</a>
+		<a href="/tui_jian?day=3&chengjiao0=0.6&chengjiao1=1&zuidi=7&zuigao=25&zhangfu0=5&zhangfu1=5&zhangfu2=8&zhangfu3=10">今日推荐</a>
 	</div>
 	<div class="form-group">
 		<span>服务器通信状态：</span>
