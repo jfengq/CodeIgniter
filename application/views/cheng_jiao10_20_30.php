46c8e39a725b9e46643d61bb849759f40ac0a0e7
micro <648580211@qq.com>
2016-09-04 22:00:24 +0800

    Create cheng_jiao10_20_30.php

---
+++ application/views/cheng_jiao10_20_30.php
@@ -0,0 +1,33 @@
+<?php include 'templates/head.php';?>
+<div class="body">
+    <h1>10,20,30日成交推荐列表</h1>
+    <div class="form-group">
+        <a href="/">首页</a>
+    </div>
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th>
+                代码
+            </th>
+            <th>
+                名称
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($chengjiao10_20_30 as $stock): ?>
+            <tr>
+                <td>
+                    <?php echo substr($stock['code'], 2); ?>
+                </td>
+                <td>
+                    <?php echo $stock['name']; ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+    <script>
+    </script>
+</div>
