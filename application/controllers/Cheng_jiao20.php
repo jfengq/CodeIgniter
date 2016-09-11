b1afeaadea3776c58b4e7ac6ed97400844efa3e8
quan.jf <quan.jf@yuwan8.com>
2016-09-11 19:24:45 +0800

    update condation

--- application/controllers/Cheng_jiao20.php
+++ application/controllers/Cheng_jiao20.php
@@ -23,7 +23,7 @@ class Cheng_Jiao20 extends CI_Controller
             $min = min($cj['day0'], $cj['day1'], $cj['day2'], $cj['day3'], $cj['day4'], $cj['day5'], $cj['day6'], $cj['day7'], $cj['day8'], $cj['day9']
                 , $cj['day10'], $cj['day11'], $cj['day12'], $cj['day13'], $cj['day14'], $cj['day15'], $cj['day16'], $cj['day17'], $cj['day18'], $cj['day19']);
             
-            if ($min > 0 && $min == $cj['day0'] && $cj['day0'] != 100000000) {
+            if ($min > 0 && $min*1.1 >= $cj['day0'] && $cj['day0'] != 100000000) {
                 array_push($result_arr, $cj);
             }
         }
