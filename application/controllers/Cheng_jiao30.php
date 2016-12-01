69125d849f2a35cce491a467994d5bd4c25e73f3
micro <648580211@qq.com>
2016-12-01 15:59:52 +0800

    Update Cheng_jiao30.php

--- application/controllers/Cheng_jiao30.php
+++ application/controllers/Cheng_jiao30.php
@@ -19,8 +19,11 @@ class Cheng_Jiao30 extends CI_Controller
             $min = min($cj['day0'], $cj['day1'], $cj['day2'], $cj['day3'], $cj['day4'], $cj['day5'], $cj['day6'], $cj['day7'], $cj['day8'], $cj['day9']
                 , $cj['day10'], $cj['day11'], $cj['day12'], $cj['day13'], $cj['day14'], $cj['day15'], $cj['day16'], $cj['day17'], $cj['day18'], $cj['day19']
                 , $cj['day20'], $cj['day21'], $cj['day22'], $cj['day23'], $cj['day24'], $cj['day25'], $cj['day26'], $cj['day27'], $cj['day28'], $cj['day29']);
+            $max = max($cj['day0'], $cj['day1'], $cj['day2'], $cj['day3'], $cj['day4'], $cj['day5'], $cj['day6'], $cj['day7'], $cj['day8'], $cj['day9']
+                , $cj['day10'], $cj['day11'], $cj['day12'], $cj['day13'], $cj['day14'], $cj['day15'], $cj['day16'], $cj['day17'], $cj['day18'], $cj['day19']
+                , $cj['day20'], $cj['day21'], $cj['day22'], $cj['day23'], $cj['day24'], $cj['day25'], $cj['day26'], $cj['day27'], $cj['day28'], $cj['day29']);
             
-            if ($min > 0 && $min >= $cj['day0'] && $cj['day0'] != 100000000) {
+            if ($min > 0 && $min >= $cj['day0'] && $max > 3 * $min && $cj['day0'] != 100000000) {
                 array_push($result_arr, $cj);
             }
         }
